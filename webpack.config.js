740f3c468817e12f325aa0e73c506f05fdf29f29
gegorov <gegorov@yahoo.com>
2019-07-22 00:49:47 +0300

    add splitChunks

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,19 @@ const htmlWebpackPlugin =require('html-webpack-plugin');
 
 const BUILD_DIR = path.join(__dirname, 'dist');
 const APP_DIR = path.join(__dirname, 'src');
+const VENDOR_LIBS = ['react', 'react-dom', 'react-router-dom'];
 
 
 const config = {
   // mode: "development",
-  entry: `${APP_DIR}/index.js`,
+  // entry: `${APP_DIR}/index.js`,
+  entry: {
+    bundle: `${APP_DIR}/index.js`,
+    vendor: VENDOR_LIBS,
+  },
   output: {
     path: BUILD_DIR,
-    filename: 'app.bundle.js',
+    filename: '[name].js',
   },
   module: {
     rules: [
@@ -51,6 +56,11 @@ const config = {
       title: 'My Reads from LinkedIn Learning Course',
     }),
   ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 }
 
 module.exports = config;
