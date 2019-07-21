5fe9e06aebebc73a980d10d17fc7b159906bfa62
gegorov <gegorov@yahoo.com>
2019-07-22 00:26:19 +0300

    add and configure htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const htmlWebpackPlugin =require('html-webpack-plugin');
 
 const BUILD_DIR = path.join(__dirname, 'dist');
 const APP_DIR = path.join(__dirname, 'src');
@@ -43,7 +44,13 @@ const config = {
         use: 'file-loader',
       },
     ],
-  }
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: 'src/index.html',
+      title: 'My Reads from LinkedIn Learning Course',
+    }),
+  ],
 }
 
 module.exports = config;
