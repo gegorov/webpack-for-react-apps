8f47fed3b61874ffb222e8b5cb894e218a8fd727
gegorov <gegorov@yahoo.com>
2019-07-22 11:59:11 +0300

    prepare for buildin to gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -80,6 +80,9 @@ const config = {
     }),
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
   ],
   optimization: {
     splitChunks: {
